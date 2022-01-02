57208a2bc1a7b089be417cf4a363120220f1ca1d
Cansu Yarkın <93792326+cansuyarkin@users.noreply.github.com>
2022-01-02 14:18:48 +0300

    Update Footer.jsx

--- first_project/src/components/base/Footer.jsx
+++ first_project/src/components/base/Footer.jsx
@@ -6,26 +6,26 @@ function Footer (props) {
       <div className="col-6 col-md mx-5">
         <h5>Films</h5>
         <ul className="list-unstyled text-small">
-          <li className="mb-1"><a className="link-secondary text-decoration-none" >Film Title</a></li>
-          <li className="mb-1"><a className="link-secondary text-decoration-none" >Release Date</a></li>
-          <li className="mb-1"><a className="link-secondary text-decoration-none" >On the air this week</a></li>
+          <li className="mb-1"><p className="link-secondary text-decoration-none" >Film Title</p></li>
+          <li className="mb-1"><p className="link-secondary text-decoration-none" >Release Date</p></li>
+          <li className="mb-1"><p className="link-secondary text-decoration-none" >On the air this week</p></li>
           
         </ul>
       </div>
       <div className="col-6 col-md mx-5">
         <h5>Film Details</h5>
         <ul className="list-unstyled text-small">
-          <li className="mb-1"><a className="link-secondary text-decoration-none" >Resource</a></li>
-          <li className="mb-1"><a className="link-secondary text-decoration-none" >Resource name</a></li>
+          <li className="mb-1"><p className="link-secondary text-decoration-none" >Resource</p></li>
+          <li className="mb-1"><p className="link-secondary text-decoration-none" >Resource name</p></li>
         </ul>
       </div>
       <div className="col-6 col-md mx-5">
         <h5>About</h5>
         <ul className="list-unstyled text-small">
-          <li className="mb-1"><a className="link-secondary text-decoration-none" >Team</a></li>
-          <li className="mb-1"><a className="link-secondary text-decoration-none" >Locations</a></li>
-          <li className="mb-1"><a className="link-secondary text-decoration-none" >Privacy</a></li>
-          <li className="mb-1"><a className="link-secondary text-decoration-none" >Terms</a></li>
+          <li className="mb-1"><p className="link-secondary text-decoration-none" >Team</p></li>
+          <li className="mb-1"><p className="link-secondary text-decoration-none" >Locations</p></li>
+          <li className="mb-1"><p className="link-secondary text-decoration-none" >Privacy</p></li>
+          <li className="mb-1"><p className="link-secondary text-decoration-none" >Terms</p></li>
         </ul>
       </div>
     </div>
